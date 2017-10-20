a76d3b11e7cd4e48584a418239a71a683ce6069a
Bryan Moreno <morenobryan91@gmail.com>
2017-10-20 01:12:54 -0200

    Make the app listen to the android back button

--- src/Router/components/AppNavigator.js
+++ src/Router/components/AppNavigator.js
@@ -1,15 +1,37 @@
 // @flow
 import * as React from 'react';
-import { addNavigationHelpers } from 'react-navigation';
+import { BackHandler } from 'react-native';
+import { addNavigationHelpers, NavigationActions } from 'react-navigation';
 import { Root } from '../router';
 
-const AppNavigator = props => (
-  <Root
-    navigation={addNavigationHelpers({
-      dispatch: props.dispatch,
-      state: props.nav,
-    })}
-  />
-);
+export default class AppNavigator extends React.Component {
+  componentDidMount() {
+    BackHandler.addEventListener('hardwareBackPress', this.onBackPress);
+  }
 
-export default AppNavigator;
+  componentWillUnmount() {
+    BackHandler.removeEventListener('hardwareBackPress', this.onBackPress);
+  }
+
+  onBackPress = () => {
+    const { dispatch, nav } = this.props;
+    if (nav.index === 0) {
+      return false;
+    }
+    dispatch(NavigationActions.back());
+    return true;
+  };
+
+  render() {
+    const { dispatch, nav } = this.props;
+
+    return (
+      <Root
+        navigation={addNavigationHelpers({
+          dispatch,
+          state: nav,
+        })}
+      />
+    );
+  }
+}
